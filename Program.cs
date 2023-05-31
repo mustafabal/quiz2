35ad35c81243059e5aefa43f72bc97714c96a7d5
mustafabal <balbalo@hotmail.com>
2023-05-31 11:36:12 +0300

    Flo quiz2

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Quiz2;
+
+Console.WriteLine("---------------------------------------------");
+List<Person> list = new List<Person>
+{
+    new Person { Id = 1, Name = "Ahmet Kaya", Nation = "TR", WeeklyOffDayNumber = 0, WeeklyWorkHour = 30 },
+    new Person { Id = 1, Name = "Ahmet Kaya", Nation = "TR", WeeklyOffDayNumber = 1, WeeklyWorkHour = 40 },
+    new Person { Id = 1, Name = "Mehmet Kaya", Nation = "TR", WeeklyOffDayNumber = 1, WeeklyWorkHour = 40 },
+    new Person { Id = 1, Name = "Julia", Nation = "MBB", WeeklyOffDayNumber = 8, WeeklyWorkHour = 38 },
+    new Person { Id = 1, Name = "Michale", Nation = "MBB", WeeklyOffDayNumber = 2, WeeklyWorkHour = 35 },
+    new Person { Id = 1, Name = "Ayşe", Nation = "UK", WeeklyOffDayNumber = 1, WeeklyWorkHour = 45 },
+    new Person { Id = 1, Name = "Ali", Nation = "UK", WeeklyOffDayNumber = 2.7, WeeklyWorkHour = 77 },
+    new Person { Id = 1, Name = "Ömer", Nation = "Italy", WeeklyOffDayNumber = 4, WeeklyWorkHour = 55 },
+    new Person { Id = 1, Name = "Mustafa", Nation = "italy", WeeklyOffDayNumber = 2, WeeklyWorkHour = 30 }
+};
+
+
+WorkRule wr = new WorkRule();
+foreach (var item in list)
+{
+    var message = wr.PersonRulesCheck(item);
+    message.ForEach(msg => Console.WriteLine(msg));
+}
+
---
+++ WorkRule.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz2
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int WeeklyWorkHour { get; set; }
+        public double WeeklyOffDayNumber { get; set; }
+        public string? Nation { get; set; }
+    }
+
+    public class WorkRule
+    {
+        private Dictionary<string, double> leaveDaysByCountry = new Dictionary<string, double>()
+    {
+        { "italy", 3 },
+        { "uk", 2.5 },
+        { "tr", 1 },
+        { "mbb", 2 }
+    };
+
+        private Dictionary<string, int> workingHoursByCountry = new Dictionary<string, int>()
+    {
+        { "italy", 30 },
+        { "uk", 30 },
+        { "tr", 45 },
+        { "mbb", 40 }
+    };
+
+        public List<string> PersonRulesCheck(Person person)
+        {
+            var culture = new CultureInfo("en-EN");
+            List<string> messages = new List<string>();
+
+            if (leaveDaysByCountry.ContainsKey(key: person.Nation.ToLower(culture)))
+            {
+                double leaveDays = leaveDaysByCountry[person.Nation.ToLower(culture)];
+                int workingHours = workingHoursByCountry[person.Nation.ToLower(culture)];
+
+                if (person.WeeklyOffDayNumber > leaveDays)
+                {
+                    messages.Add(person.Name + ": için Haftalık en az " + leaveDays + " gün izin olması gerekir.");
+                }
+
+                if (person.WeeklyWorkHour > workingHours)
+                {
+                    messages.Add(person.Name + ": için Haftalık çalışma saati " + workingHours + " saatten fazla olamaz.");
+                }
+
+                return messages;
+            }
+
+            messages.Add("Invalid nation: " + person.Nation);
+            return messages;
+        }
+
+    }
+}
